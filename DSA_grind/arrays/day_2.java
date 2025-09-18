bd5f44a68d5a4d629c832f906427c06620c45e21
K V L N Kushal <kls2edmentre@gmail.com>
2025-09-18 23:56:57 +0530

    2 questions with brute and optimized ways

--- DSA_grind/arrays/day_2.java
+++ DSA_grind/arrays/day_2.java
@@ -1,4 +1,8 @@
 package arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class day_2 {
     static void func_1(){
         int[] arr = {1, 2, 3, 6, 4};
@@ -12,13 +16,46 @@ public class day_2 {
             System.out.println("Array is not sorted");
         }
     }
-    
+
     static void func_2(){
 
+        // removing duplicates from sorted array
+        int[] arr = {1,1,2,2,2,2,2,3,3,3,4,4,4,4,4};
+        //brute force
+        // via a set
+        int n = arr.length;
+        Set<Integer> s = new HashSet<>();
+        for(int i=0;i<n;i++){
+            s.add(arr[i]);
+        }
+        for(int num : s){
+            System.out.print(num+" ");
+        }
+        System.out.println("");
+    }
+
+    static void q2_optimized(){
+        int[] arr = {1,1,2,2,2,2,2,3,3,3,4,4,4,4,4};
+        int n = arr.length;
+        int i=0;
+        for(int j=1;j<n;j++){
+            if(arr[i] == arr[j]){
+                arr[i+1] = arr[j];
+            }
+            else{
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        for(int k=0;k<=i;k++){
+            System.out.print(arr[k]+" ");
+        }
+    System.out.println(" ");
     }
 
     public static void main(String[] args) {
         func_1();
         func_2();
+        q2_optimized();
     }
 }
